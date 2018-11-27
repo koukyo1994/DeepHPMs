540af2cfd3d45f64026b7cc88064dc3f724a0e45
koukyo1994 <arabiannight1994@yahoo.co.jp>
2018-11-27 14:12:33 +0900

    sine to polynominal

--- Mine/config/constants.py
+++ Mine/config/constants.py
@@ -2,5 +2,5 @@ import tensorflow as tf
 
 ACTIVATION = tf.tanh
 INTERVAL = 100
-LOG_PATH = "log/multiple_train_cos_sine_predict_polynominal.log"
+LOG_PATH = "log/multiple_train_sine_predict_polynominal.log"
 
